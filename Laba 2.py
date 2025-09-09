580dcfef7343855b8871328cd9a6f7a247e0918d
Andriy-Anti-Anime <savulaandrij84@gmail.com>
2025-09-09 21:56:12 +0300

    Laba 2.py

--- Laba 2.py
+++ Laba 2.py
@@ -1,5 +1,5 @@
-main_list = [12, 5, 7, 20, 3, 18, 1, 25, 14, 9,
-             "banana", "apple", "grape", "cherry", "orange", "pear", "kiwi", "plum", "peach", "mango"]
+main_list = [10, 20, 3, 8, 1, 4, 2, 4, 17, 12, 11,
+             "banana", "apple", "grape", "cherry", "orange", "pear", "kiwi", "plum", "peach", "mango", "strawberry"]
 
 ints = sorted([x for x in main_list if isinstance(x, int)])
 strings = sorted([x for x in main_list if isinstance(x, str)])
@@ -13,4 +13,5 @@ uppercased_strings = [x.upper() for x in strings]
 print("Основний список:", main_list)
 print("Відсортований список:", sorted_list)
 print("Елементи, кратні двом:", multiples_of_two)
+
 print("Рядки капсом:", uppercased_strings)
